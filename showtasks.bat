45b9260b0f8f82ac43a46e6ed941d1412bc15182
“mkosecki1 <mkosecki@op.pl>
2017-12-29 18:17:12 +0100

    17.4.

--- showtasks.bat
+++ showtasks.bat
@@ -2,19 +2,19 @@ call runcrud.bat
 
 if "%ERRORLEVEL%" == "0" goto webBrowser
 echo.
-echo There were errors with runcrud - breaking work
+echo There were errors with runcrud.
 goto fail
 
 :webBrowser
 @start http://localhost:8080/crud/v1/task/getTasks
 if "%ERRORLEVEL%" == "0" goto end
 echo.
-echo There were errors with web browser - breaking work
+echo There were errors with web browser.
 goto fail
 
 :fail
 echo.
-echo There were errors
+echo There were errors.
 
 :end
 echo.
