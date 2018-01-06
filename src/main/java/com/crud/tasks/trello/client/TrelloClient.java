330a660cae94e7f16fa7b882baf8215eab83b342
“mkosecki1 <mkosecki@op.pl>
2018-01-06 21:26:06 +0100

    zadanie 18.2. update

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -35,24 +34,10 @@ public class TrelloClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    /*
     public List<TrelloBoardDto> getTrelloBoards(){
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
-                .queryParam(KEY,trelloAppKey)
-                .queryParam(TOKEN, trelloToken)
-                .queryParam(FIELDS,"name,id").build().encode().toUri();
-
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
-
-        /*
-        if(boardsResponse != null){
-            return Arrays.asList(boardsResponse);
-        }
-        */
-        //return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
-        //return new ArrayList<>();
-
-    //}
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(createTrelloBoardsURI(), TrelloBoardDto[].class);
+        return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
+    }
 
     private URI createTrelloBoardsURI(){
         return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
@@ -61,8 +46,5 @@ public class TrelloClient {
                 .queryParam(FIELDS,"name,id").build().encode().toUri();
     }
 
-    public List<TrelloBoardDto> getTrelloBoards(){
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(createTrelloBoardsURI(), TrelloBoardDto[].class);
-        return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
-    }
+
 }
