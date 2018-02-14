e37dedee50128780a62cfdeabfffc222fa1fa03b
“mkosecki1 <mkosecki@op.pl>
2018-02-14 18:33:29 +0100

    zadanie 24.1.

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -9,8 +9,14 @@ import java.util.Map;
 public class StaticWebPageController {
 
     @RequestMapping("/")
-    public String index(Map<String,String> model){
+    public String index(Map<String,Object> model){
         model.put("variable","My Thymeleaf variable");
+        model.put("one",1);
+        model.put("two",2);
+        model.put("multiplication","*");
+        model.put("equals","=");
+        model.put("add","+");
+        model.put("subtract","-");
         return "index";
     }
 }
