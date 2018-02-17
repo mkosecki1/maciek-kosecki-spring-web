505236438046e911ab16fa7abb710602e713dd26
“mkosecki1 <mkosecki@op.pl>
2018-02-17 12:10:56 +0100

    zadanie 24.2.

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
-
     @Autowired
     private AdminConfig adminConfig;
 
@@ -18,14 +20,39 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message){
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
-        context.setVariable("message",message);
-        context.setVariable("tasks_url","http://localhost:8888/tasks_frontend/");
-        context.setVariable("button","Visit website");
-        context.setVariable("admin_name",adminConfig.getAdminname());
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
         context.setVariable("goodbye", "goodbye");
         context.setVariable("company_details", "COMPANY_DETAILS");
         context.setVariable("is_friend", false);
-        return templateEngine.process("mail/created-trello-card-mail",context);
+        context.setVariable("show_button", false);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildAnotherEmail(String message){
+        List<String> functionality = new ArrayList<>();
+        functionality.add("Provides connection");
+        functionality.add("Application allows sending tasks");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("goodbye", "goodbye");
+        context.setVariable("company_details", "COMPANY_DETAILS");
+        context.setVariable("is_friend", false);
+        context.setVariable("show_button", false);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/create-mail", context);
     }
 }
